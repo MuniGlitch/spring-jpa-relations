28ad975d789ac3456a28ead38cdf2d026ebe3541
MuniGlitch <chni0006@stud.kea.dk>
2025-08-29 09:57:14 +0200

    Completed exercise 01

--- src/main/java/ek/osnb/jpa/orders/repository/OrderLineRepository.java
+++ src/main/java/ek/osnb/jpa/orders/repository/OrderLineRepository.java
@@ -1,4 +1,6 @@
 package ek.osnb.jpa.orders.repository;
+import ek.osnb.jpa.orders.model.OrderLine;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderLineRepository {
+public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
 }
